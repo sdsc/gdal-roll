5a222191f6ea8a84f3638950676b6c7d38bcf851
Trevor Cooper <tcooper@sdsc.edu>
2013-11-20 16:29:22 -0800

    Updated Makefile and version.mk to use pull.mk

--- src/gdal/version.mk
+++ src/gdal/version.mk
@@ -1,3 +1,14 @@
-NAME    = gdal
-VERSION = 1.9.2
-RELEASE = 1
+NAME               = gdal
+VERSION            = 1.9.2
+RELEASE            = 1
+PKGROOT            = /opt/gdal
+
+SRC_SUBDIR         = gdal
+
+SOURCE_NAME        = $(NAME)
+SOURCE_VERSION     = $(VERSION)
+SOURCE_SUFFIX      = tar.gz
+SOURCE_PKG         = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
+SOURCE_DIR         = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
+
+TAR_GZ_PKGS        = $(SOURCE_PKG)
